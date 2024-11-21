0cd48cb7a3a06cc1caf0867b6a27f51c39e061e8
asin_CLX <asin2002@163.com>
2024-11-21 18:06:44 +0800

    feat: 登录调整

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,10 +1,25 @@
+import LogoImage from '@/assets/img/logo.png'
+import { DEFAULT_NAME } from '@/constants'
 import { classNameBind } from '@/utils/classnamesBind'
+import { Helmet } from '@umijs/max'
 import style from './index.less'
 
 const cx = classNameBind(style)
 
 const Login: React.FC = () => {
-  return <div className={cx('container')}> 这是 Login 组件 </div>
+  return (
+    <div className={cx('container')}>
+      <Helmet>
+        <title>登录 - {DEFAULT_NAME}</title>
+      </Helmet>
+      <div className={cx('header')}>
+        <div className={cx('logo')}>
+          <img className={cx('logo')} src={LogoImage} />
+        </div>
+        <div className={cx('title')}>登录 {DEFAULT_NAME}</div>
+      </div>
+    </div>
+  )
 }
 
 export default Login
