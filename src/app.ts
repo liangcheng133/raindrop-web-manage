ce547ae9d1d52cbbf54f2915cf0ce741a06a3b8f
asin_CLX <asin2002@163.com>
2024-11-22 17:26:10 +0800

    feat: 面包屑增加首页

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,25 @@
 // 运行时配置
 
-import { DEFAULT_NAME } from "./constants";
+import { RunTimeLayoutConfig } from '@umijs/max'
+import { BreadcrumbProps } from 'antd/lib'
+import { DEFAULT_NAME } from './constants'
 
 // 全局初始化数据配置，用于 Layout 用户信息和权限初始化
 // 更多信息见文档：https://umijs.org/docs/api/runtime-config#getinitialstate
 export async function getInitialState(): Promise<{ name: string }> {
-  return { name: DEFAULT_NAME };
+  return { name: DEFAULT_NAME }
 }
 
-export const layout = () => {
+export const layout: RunTimeLayoutConfig = () => {
   return {
     title: DEFAULT_NAME,
     logo: 'https://img.alicdn.com/tfs/TB1YHEpwUT1gK0jSZFhXXaAtVXa-28-27.svg',
     menu: {
-      locale: false, // 关闭菜单国际化
+      locale: false // 关闭菜单国际化
     },
-  };
-};
+    breadcrumbRender: (routers: BreadcrumbProps[]) => {
+      console.log('[ routers ] >', routers)
+      return [{ path: '/', breadcrumbName: '首页' }, ...routers]
+    }
+  }
+}
