07e0742e9765263fee881e0ed4c7b6272a62d2b9
asin_CLX <asin2002@163.com>
2025-01-13 18:06:57 +0800

    feat(User): 优化用户列表展示效果
    - 移除了未使用的 Button 组件导入
    - 在用户列表中添加了操作列
    - 为表格添加了滚动条和虚拟滚动功能，提高大数据量下的性能和用户体验

--- src/pages/User/index.tsx
+++ src/pages/User/index.tsx
@@ -1,7 +1,6 @@
 import useTable from '@/hooks/useTable'
 import { classNameBind } from '@/utils/classnamesBind'
 import { PageContainer, ProColumns, ProTable } from '@ant-design/pro-components'
-import { Button } from 'antd'
 import React from 'react'
 import styles from './index.less'
 
@@ -13,18 +12,21 @@ const columns: ProColumns[] = [
   { title: '手机号', dataIndex: 'phone' },
   { title: '邮箱', dataIndex: 'email' },
   { title: '创建时间', dataIndex: 'create_time' },
-  { title: '修改时间', dataIndex: 'update_time' }
+  { title: '修改时间', dataIndex: 'update_time' },
+  { dataIndex: 'operation' }
 ]
 
 const UserList: React.FC = () => {
   const tableProps = useTable<API.SystemUser, API.SystemUserQuery>({
     api: '/sys/user/list',
-    columns: columns
+    columns: columns,
+    scroll: { y: 300 },
+    virtual: true
   })
-
   return (
     <PageContainer ghost>
       <ProTable<API.SystemUser> {...tableProps} />
+      
     </PageContainer>
   )
 }
