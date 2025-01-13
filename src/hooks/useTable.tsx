96d52e23c0c36f829b271d40a5f8b4592d75acd9
asin_CLX <asin2002@163.com>
2025-01-13 18:06:44 +0800

    refactor(table): 优化表格组件并提升性能
    - 将 useTable.ts 重命名为 useTable.tsx
    - 引入 React 而不是单独的 useRef
    - 添加 TdCell 组件以优化表格单元格渲染性能
    - 在 ProTable 组件中集成 TdCell
    - 简化 handleColumns 函数中的 baseColumns 定义

--- src/hooks/useTable.tsx
+++ src/hooks/useTable.tsx
@@ -1,6 +1,6 @@
 import { ProTableProps } from '@ant-design/pro-table'
 import { request } from '@umijs/max'
-import { useRef } from 'react'
+import React, { useRef } from 'react'
 
 // 泛用json [key: string]: string;
 
@@ -26,6 +26,12 @@ export type UseTableType<T, U> = ProTableProps<T, U> & {
 /** 默认的 rowKey */
 const DEFAULT_ROW_KEY = 'id'
 
+const TdCell = (props: any) => {
+  // onMouseEnter, onMouseLeave在数据量多的时候，会严重阻塞表格单元格渲染，严重影响性能
+  const { onMouseEnter, onMouseLeave, ...restProps } = props
+  return <td {...restProps} />
+}
+
 /**
  * 封装 ProTable 常用的属性 与 请求方法
  */
@@ -38,6 +44,9 @@ export default function useTable<T, U>(options: UseTableType<T, U>): ProTablePro
     ...rest,
     rowKey,
     columns,
+    components: {
+      body: TdCell
+    },
     request: async (requestParams) => {
       const params = handleTableParams(requestParams, options)
       console.log('[ params ] >', params)
@@ -74,9 +83,7 @@ function handleTableParams<T, U>(requestParams: U, { handleParams }: UseTableTyp
 
 /** 处理 columns */
 function handleColumns<T, U>({ columns: propsColumns }: UseTableType<T, U>) {
-  const baseColumns = {
-    ellipsis: true
-  }
+  const baseColumns = {}
   const columns = propsColumns?.map((item) => {
     const newItem = { ...baseColumns, ...item }
     return newItem
