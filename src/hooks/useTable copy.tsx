160713d07c5754c57776d1d75b3acf83a6749632
asin_CLX <asin2002@163.com>
2025-05-28 17:51:41 +0800

    feat: useTable重构中

---
+++ src/hooks/useTable copy.tsx
@@ -0,0 +1,223 @@
+import { objRemoveEmpty } from '@/utils'
+import { ColumnsState, ProColumns, TableDropdown } from '@ant-design/pro-components'
+import { ProTableProps, RequestData } from '@ant-design/pro-table'
+import { DropdownProps } from '@ant-design/pro-table/es/components/Dropdown/index'
+import { useSafeState } from 'ahooks'
+import { Popconfirm } from 'antd'
+import { throttle } from 'es-toolkit'
+import React, { ReactNode, useEffect, useRef } from 'react'
+import 'react-resizable/css/styles.css' // 引入默认样式
+import { ColumnOptions, RenderFunctionParams, RequestFunctionParams, UseTableColumnsType, UseTableType } from './type'
+
+/** 默认的列宽 */
+const DEFAULT_COLUMN_WIDTH = 120
+/** 默认表格高度 */
+const DEFAULT_TABLE_HEIGHT = 400
+/** 默认表格配置 */
+const DEFAULT_PROPS = {
+  rowKey: 'id',
+  resizable: true,
+  clearEmptyParams: true
+}
+
+/** 封装 ProTable 常用的属性 与 请求方法 */
+export default function useTable<T>(useTableProps: UseTableType<T, any>): ProTableProps<T, any> {
+  const [tableBodyHeight, setTableBodyHeight] = useSafeState(DEFAULT_TABLE_HEIGHT)
+  const options = {
+    ...DEFAULT_PROPS,
+    scroll: { x: 'max-content', y: tableBodyHeight },
+    ...useTableProps
+  }
+  const {
+    api,
+    handleParams,
+    clearEmptyParams,
+    columns: propsColumns,
+    tableLayout = 'fixed',
+    persistenceColumnsKey,
+    ...rest
+  } = options
+
+  const [columnOptions, setColumnOptions] = useSafeState<ColumnOptions>({})
+  const columnsStateValue = useRef<Record<string, ColumnsState>>({}) // 持久化列配置数据
+  const oldResponse = useRef<RequestData<T> | null>(null) // 上次成功的请求结果
+
+  const columns = handleColumns(options, columnOptions, setColumnOptions) // 列配置
+
+  /** 更新表格高度 */
+  const refreshTableHeight = () => {
+    setTableBodyHeight(calculationTableBodyHeight())
+  }
+
+  useEffect(() => {
+    refreshTableHeight()
+    window.onresize = throttle(refreshTableHeight, 300)
+    return () => {
+      window.onresize = null
+    }
+  }, [])
+
+  return {
+    ...rest,
+    columns,
+    components: {},
+    columnsState: persistenceColumnsKey
+      ? {
+          onChange: (value: Record<string, ColumnsState>) => {
+            columnsStateValue.current = value
+          },
+          persistenceKey: persistenceColumnsKey,
+          persistenceType: 'localStorage'
+        }
+      : undefined,
+    request: async (...args) => {
+      const params = handleTableParams(...args, options)
+
+      try {
+        const res = (await api(params)) as RequestData<T>
+
+        const response = {
+          data: res.data,
+          total: res.total,
+          success: true
+        }
+        oldResponse.current = response
+
+        return response
+      } catch (error) {
+        console.log('[ useTable 的 request 报错了 ] >', error)
+        return {
+          ...oldResponse?.current,
+          success: false
+        }
+      }
+    }
+  }
+}
+
+/** 处理请求参数 */
+function handleTableParams<T, U>(
+  params: RequestFunctionParams<T, U>[0],
+  sort: RequestFunctionParams<T, U>[1],
+  filter: RequestFunctionParams<T, U>[2],
+  { handleParams, clearEmptyParams }: UseTableType<T, U>
+) {
+  const { current, pageSize, ...rest } = params
+  let requestParams = { ...rest, current: current, count: pageSize }
+  if (handleParams) {
+    requestParams = handleParams(requestParams, sort, filter)
+  }
+  if (clearEmptyParams) {
+    objRemoveEmpty(requestParams)
+  }
+  return requestParams
+}
+
+/** 处理 columns */
+function handleColumns<T, U>(
+  { columns: propsColumns, resizable }: UseTableType<T, U>,
+  columnOptions: ColumnOptions,
+  setColumnOptions: (value: ColumnOptions) => void
+) {
+  const columns = propsColumns?.map((item) => {
+    const { ellipsis = true, onHeaderCell, ...rest } = item
+    const extra: ProColumns<T> = {
+      width: DEFAULT_COLUMN_WIDTH
+    }
+
+    if (item.valueType === 'option') {
+      Object.assign(extra, {
+        title: '操作',
+        fixed: 'right',
+        render: (...args) => renderColumnOperation(item, ...args)
+      } as ProColumns)
+    }
+
+    if (resizable && !onHeaderCell) {
+      // extra.width = columnOptions[resizableKey]?.width ?? width
+    }
+
+    return {
+      ...extra,
+      ...rest
+    }
+  })
+  return columns
+}
+
+/** 处理 columns 操作列 */
+function renderColumnOperation<T>(
+  column: UseTableColumnsType<T>,
+  dom: RenderFunctionParams<T>[0],
+  record: RenderFunctionParams<T>[1],
+  index: RenderFunctionParams<T>[2],
+  action: RenderFunctionParams<T>[3],
+  schema: RenderFunctionParams<T>[4]
+): ReactNode {
+  const renderOperation = column.renderOperation
+  if (renderOperation) {
+    const renderOperations = renderOperation(dom, record, index, action, schema)
+    const buttonsRender: ReactNode[] = []
+    const dropdownItems: DropdownProps['menus'] = []
+    const operationMaxShowQuantity = column.operationMaxShowQuantity ?? 3
+
+    renderOperations.forEach((rItem) => {
+      const { name, key, hide, outside, type, onClick, ...rest } = rItem
+      const callClick = () => {
+        return onClick?.()
+      }
+      if (hide !== true) {
+        if (buttonsRender.length < operationMaxShowQuantity - 1 || outside) {
+          if (type === 'deleteConfirm') {
+            buttonsRender.push(
+              <Popconfirm
+                title='删除提示'
+                description='您确定要删除该数据吗？'
+                onConfirm={callClick}
+                key={key}
+                okText='确认'
+                cancelText='取消'>
+                <a className='danger-color'>{name}</a>
+              </Popconfirm>
+            )
+          } else {
+            buttonsRender.push(
+              <a key={key} onClick={callClick}>
+                {name}
+              </a>
+            )
+          }
+        } else {
+          dropdownItems.push({ key: key, name, ...rest })
+        }
+      }
+    })
+    const renders = [...buttonsRender]
+    if (dropdownItems.length > 0) {
+      renders.push(
+        <TableDropdown
+          key='actionGroup'
+          onSelect={(key) => {
+            renderOperations.find((item) => item.key === key)?.onClick?.()
+          }}
+          menus={dropdownItems}
+        />
+      )
+    }
+    return renders
+  }
+}
+
+/**
+ * 计算表格可用高度
+ * @param threshold 阈值，高度会减少该阈值 默认为100
+ * @returns
+ */
+function calculationTableBodyHeight(threshold = 100) {
+  const clientHeight = document.documentElement.clientHeight
+  const bodyArr = document.getElementsByClassName('ant-table-body')
+  if (bodyArr.length) {
+    return clientHeight - bodyArr[0].getBoundingClientRect().top - threshold
+  }
+  return DEFAULT_TABLE_HEIGHT
+}
--- src/hooks/useTable.tsx
+++ src/hooks/useTable.tsx
@@ -1,219 +1,123 @@
-import { objRemoveEmpty } from '@/utils'
-import { ColumnsState, ProColumns, TableDropdown } from '@ant-design/pro-components'
-import { ProTableProps, RequestData } from '@ant-design/pro-table'
-import { DropdownProps } from '@ant-design/pro-table/es/components/Dropdown/index'
+import { ProColumns } from '@ant-design/pro-components'
+import { ProTableProps } from '@ant-design/pro-table'
 import { useSafeState } from 'ahooks'
-import { Popconfirm } from 'antd'
-import { throttle } from 'es-toolkit'
-import React, { ReactNode, useEffect, useRef } from 'react'
-import 'react-resizable/css/styles.css' // 引入默认样式
-import { ColumnOptions, RenderFunctionParams, RequestFunctionParams, UseTableColumnsType, UseTableType } from './type'
+import { TableProps } from 'antd'
+import { cloneDeep, throttle } from 'es-toolkit'
+import { useEffect } from 'react'
 
-/** 默认的列宽 */
-const DEFAULT_COLUMN_WIDTH = 120
-/** 默认表格高度 */
-const DEFAULT_TABLE_HEIGHT = 400
-/** 默认表格配置 */
-const DEFAULT_PROPS = {
-  rowKey: 'id',
-  resizable: true,
-  clearEmptyParams: true
+export type ColumnsType<T> = ProColumns<T>
+
+export type ScrollType = TableProps<any>['scroll'] & {
+  scrollToFirstRowOnChange?: boolean
 }
 
-/** 封装 ProTable 常用的属性 与 请求方法 */
-export default function useTable<T>(useTableProps: UseTableType<T, any>): ProTableProps<T, any> {
-  const [tableBodyHeight, setTableBodyHeight] = useSafeState(DEFAULT_TABLE_HEIGHT)
-  const options = {
-    ...DEFAULT_PROPS,
-    scroll: { x: 'max-content', y: tableBodyHeight },
-    ...useTableProps
+export type UseTableConfigType<T, U> = Omit<ProTableProps<T, U>, 'columns'> & {
+  /** 接口地址 */
+  api?: (...args: any) => Promise<any>
+  /** 处理请求响应数据 */
+  handleResponse?: (...args: any) => any
+  /** 是否在请求时清除空参数，默认为true */
+  clearEmptyParams?: boolean
+  /** 列配置 */
+  columns?: ColumnsType<T>[]
+  /** 计算表格高度阈值 */
+  calcTableHeightThreshold?: number
+  /**
+   * 持久化列配置的key，传入值时即开启持久化列配置，默认使用localStorage
+   */
+  persistenceColumnsKey?: string
+}
+
+/** 默认的列宽 */
+const DEFAULT_COLUMN_WIDTH = 150
+/** 默认表格高度 */
+const DEFAULT_TABLE_HEIGHT = 400
+
+/** 处理列配置项 */
+const handleColumns = <T, U>(config: ProTableProps<T, U>, initialConfig: UseTableConfigType<T, U>) => {
+  const { columns } = initialConfig
+  config.columns = columns
+}
+
+/** 处理表格滚动 */
+const handleScroll = <T, U>(config: ProTableProps<T, U>, initialConfig: UseTableConfigType<T, U>) => {
+  const { calcTableHeightThreshold } = initialConfig
+
+  const [height, setHeight] = useSafeState(DEFAULT_TABLE_HEIGHT) // 表格高度
+
+  const refresh = () => {
+    setHeight(calcTableHeight(calcTableHeightThreshold!))
   }
-  const {
-    api,
-    handleParams,
-    clearEmptyParams,
-    columns: propsColumns,
-    tableLayout = 'fixed',
-    persistenceColumnsKey,
-    ...rest
-  } = options
-
-  const [columnOptions, setColumnOptions] = useSafeState<ColumnOptions>({})
-  const columnsStateValue = useRef<Record<string, ColumnsState>>({}) // 持久化列配置数据
-  const oldResponse = useRef<RequestData<T> | null>(null) // 上次成功的请求结果
-
-  const columns = handleColumns(options, columnOptions, setColumnOptions) // 列配置
-
-  /** 更新表格高度 */
-  const refreshTableHeight = () => {
-    setTableBodyHeight(calculationTableBodyHeight())
-  }
-
   useEffect(() => {
-    refreshTableHeight()
-    window.onresize = throttle(refreshTableHeight, 300)
+    window.onresize = throttle(refresh, 300, { edges: ['leading'] })
     return () => {
       window.onresize = null
     }
   }, [])
 
+  config.scroll = { x: 'max-content', y: height, ...scroll }
   return {
-    ...rest,
-    columns,
-    components: {},
-    columnsState: persistenceColumnsKey
-      ? {
-          onChange: (value: Record<string, ColumnsState>) => {
-            columnsStateValue.current = value
-          },
-          persistenceKey: persistenceColumnsKey,
-          persistenceType: 'localStorage'
-        }
-      : undefined,
-    request: async (...args) => {
-      const params = handleTableParams(...args, options)
-
-      try {
-        const res = (await api(params)) as RequestData<T>
-
-        const response = {
-          data: res.data,
-          total: res.total,
-          success: true
-        }
-        oldResponse.current = response
-
-        return response
-      } catch (error) {
-        console.log('[ useTable 的 request 报错了 ] >', error)
-        return {
-          ...oldResponse?.current,
-          success: false
-        }
-      }
-    }
+    /** 更新表格高度 */
+    refresh
   }
 }
 
-/** 处理请求参数 */
-function handleTableParams<T, U>(
-  params: RequestFunctionParams<T, U>[0],
-  sort: RequestFunctionParams<T, U>[1],
-  filter: RequestFunctionParams<T, U>[2],
-  { handleParams, clearEmptyParams }: UseTableType<T, U>
-) {
-  const { current, pageSize, ...rest } = params
-  let requestParams = { ...rest, current: current, count: pageSize }
-  if (handleParams) {
-    requestParams = handleParams(requestParams, sort, filter)
-  }
-  if (clearEmptyParams) {
-    objRemoveEmpty(requestParams)
-  }
-  return requestParams
-}
-
-/** 处理 columns */
-function handleColumns<T, U>(
-  { columns: propsColumns, resizable }: UseTableType<T, U>,
-  columnOptions: ColumnOptions,
-  setColumnOptions: (value: ColumnOptions) => void
-) {
-  const columns = propsColumns?.map((item) => {
-    const { ellipsis = true, onHeaderCell, ...rest } = item
-    const extra: ProColumns<T> = {
-      width: DEFAULT_COLUMN_WIDTH
-    }
-
-    if (item.valueType === 'option') {
-      Object.assign(extra, {
-        title: '操作',
-        fixed: 'right',
-        render: (...args) => renderColumnOperation(item, ...args)
-      } as ProColumns)
-    }
-
-    if (resizable && !onHeaderCell) {
-      // extra.width = columnOptions[resizableKey]?.width ?? width
-    }
-
-    return {
-      ...extra,
-      ...rest
-    }
-  })
-  return columns
-}
-
-/** 处理 columns 操作列 */
-function renderColumnOperation<T>(
-  column: UseTableColumnsType<T>,
-  dom: RenderFunctionParams<T>[0],
-  record: RenderFunctionParams<T>[1],
-  index: RenderFunctionParams<T>[2],
-  action: RenderFunctionParams<T>[3],
-  schema: RenderFunctionParams<T>[4]
-): ReactNode {
-  const renderOperation = column.renderOperation
-  if (renderOperation) {
-    const renderOperations = renderOperation(dom, record, index, action, schema)
-    const buttonsRender: ReactNode[] = []
-    const dropdownItems: DropdownProps['menus'] = []
-    const operationMaxShowQuantity = column.operationMaxShowQuantity ?? 3
-
-    renderOperations.forEach((rItem) => {
-      const { name, key, hide, outside, type, onClick, ...rest } = rItem
-      const callClick = () => {
-        return onClick?.()
-      }
-      if (hide !== true) {
-        if (buttonsRender.length < operationMaxShowQuantity - 1 || outside) {
-          if (type === 'deleteConfirm') {
-            buttonsRender.push(
-              <Popconfirm
-                title='删除提示'
-                description='您确定要删除该数据吗？'
-                onConfirm={callClick}
-                key={key}
-                okText='确认'
-                cancelText='取消'>
-                <a className='danger-color'>{name}</a>
-              </Popconfirm>
-            )
-          } else {
-            buttonsRender.push(
-              <a key={key} onClick={callClick}>
-                {name}
-              </a>
-            )
-          }
-        } else {
-          dropdownItems.push({ key: key, name, ...rest })
-        }
-      }
-    })
-    const renders = [...buttonsRender]
-    if (dropdownItems.length > 0) {
-      renders.push(
-        <TableDropdown
-          key='actionGroup'
-          onSelect={(key) => {
-            renderOperations.find((item) => item.key === key)?.onClick?.()
-          }}
-          menus={dropdownItems}
-        />
-      )
-    }
-    return renders
+/** 处理列状态 */
+const handleColumnState = <T, U>(config: ProTableProps<T, U>, initialConfig: UseTableConfigType<T, U>) => {
+  const { persistenceColumnsKey, columnsState } = initialConfig
+  config.columnsState = {
+    persistenceKey: persistenceColumnsKey,
+    persistenceType: 'localStorage',
+    ...columnsState
   }
 }
+
+/** 处理请求 */
+const handleRequest = <T, U>(config: ProTableProps<T, U>, initialConfig: UseTableConfigType<T, U>) => {
+  return {}
+}
+
+/** 初始话配置项 */
+const handleInitialConfig = <T, U>(initialConfig?: UseTableConfigType<T, U>): UseTableConfigType<T, U> => {
+  const config = {
+    rowKey: 'id'
+  }
+  if (initialConfig) {
+    const { clearEmptyParams = true, calcTableHeightThreshold = 100, ...rest } = cloneDeep(initialConfig)
+    Object.assign(config, { ...rest, clearEmptyParams, calcTableHeightThreshold })
+  }
+  return config
+}
+
+/** antd Pro表格配置封装 */
+const useTable = <T, U>(initialConfig?: UseTableConfigType<T, U>): ProTableProps<T, U> => {
+  const tableConfig: ProTableProps<T, U> = {}
+  const initialConfigData = handleInitialConfig(initialConfig)
+
+  // 处理列配置
+  handleColumns(tableConfig, initialConfigData)
+
+  // 处理表格滚动
+  const { refresh: refreshTableScroll } = handleScroll(tableConfig, initialConfigData)
+
+  // 处理列状态
+  handleColumnState(tableConfig, initialConfigData)
+
+  // 处理请求
+  const {} = handleRequest(tableConfig, initialConfigData)
+
+  console.log('[ tableConfig ] >', tableConfig)
+  return tableConfig
+}
+
+export default useTable
 
 /**
  * 计算表格可用高度
  * @param threshold 阈值，高度会减少该阈值 默认为100
  * @returns
  */
-function calculationTableBodyHeight(threshold = 100) {
+function calcTableHeight(threshold: number) {
   const clientHeight = document.documentElement.clientHeight
   const bodyArr = document.getElementsByClassName('ant-table-body')
   if (bodyArr.length) {
